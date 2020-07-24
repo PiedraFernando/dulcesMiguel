f973c324efdfe3db72722a4f19d5edc24f54f416
ernestvalsan <vseo170522@upemor.edu.mx>
2020-07-24 14:01:51 -0500

    Agrego el CRUD de Categorias.

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Categoria;
+use Illuminate\Http\Request;
+
+class CategoriaController extends Controller
+{
+
+    public function index()
+    {
+        $categorias = Categoria::all();
+        return view( 'categorias.index', ['categorias' => $categorias]);
+    }
+
+
+    public function create()
+    {
+        return view('categorias.create');
+    }
+
+
+    public function store(Request $request)
+    {
+        $categoria =  new Categoria();
+
+        $categoria->categoria = request('categoria');
+        $categoria->sub_categoria = request('sub_categoria');
+
+        $categoria->save();
+
+        return redirect('/categorias');
+    }
+
+
+    public function show($id)
+    {
+        //
+    }
+
+
+    public function edit($id)
+    {
+        return view ('categorias.edit',['categoria'=>Categoria::findOrFail($id)]);
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        $categoria =  Categoria::findOrFail($id);
+
+        $categoria->categoria = $request->get('categoria');
+        $categoria->sub_categoria = $request->get('sub_categoria');
+
+        $categoria->update();
+
+        return redirect('/categorias');
+    }
+
+
+    public function destroy($id)
+    {
+        $categoria = Categoria::findOrFail($id);
+        $categoria->delete();
+
+        return redirect ('/categorias');
+    }
+}
